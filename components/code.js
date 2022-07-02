77f24189b3344b7d94bcf7729bf7e93f51bc75e1
yail259 <yaoke259@outlook.com>
2022-07-02 08:21:52 -0400

    fixed python post 1

---
+++ components/code.js
@@ -0,0 +1,4 @@
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/default-highlight';
+import {dracula} from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+export default SyntaxHighlighter;
